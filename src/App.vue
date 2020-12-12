0b067af411cc8eb3a7be5e961899714a1df91da1
BenchBeach <1779227906@qq.com>
2020-12-12 21:00:13 +0800

    refact: route

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,12 @@
 <template>
-  <v-app>
-    <Header @naviOpen="drawer = true" />
-    <Navigation :drawer="drawer" @naviClose="drawer = false" />
-    <Footer @naviOpen="drawer = true" />
-    <v-main class="blue-grey lighten-5">
-      <v-container>
-        <v-row justify="center">
-          <v-col xl="9" lg="10" md="12" sm="12" xs="12">
-      <router-view />
-      </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-  </v-app>
+  <router-view></router-view>
 </template>
 
 <script>
-import Footer from "@/components/footer/footer.vue";
-import Header from "@/components/header/header.vue";
-import Navigation from "@/components/navigation/navagation";
+
 
 export default {
-  name: "App",
-
-  components: {
-    Footer,
-    Header,
-    Navigation,
-  },
-
-  data: () => ({
-    drawer: false,
-  }),
+  name: "App"
 };
 </script>
 
