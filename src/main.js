9413d2f918f4bbc5b7d68ce20ba805961a095954
X_sisiphus <1821570960@qq.com>
2022-08-22 18:37:30 +0800

    修复港澳台注册的bug

--- src/main.js
+++ src/main.js
@@ -6,11 +6,6 @@ import vuetify from './plugins/vuetify';
 import Message from '@/components/message/index.js'
 // import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
-import axios from 'axios'
-Vue.prototype.$axios = axios
-axios.defaults.baseURL = '/api'  //关键代码
-Vue.config.productionTip = false
-
 Vue.prototype.$message = Message
 Vue.config.productionTip = false
 
